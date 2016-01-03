e76ed7dfff502173035595be64250fa9769c6937
jaaaaaaaaaam <j.wallen.jones@googlemail.com>
2016-01-03 00:58:27 +0000

    add struct for episode images

--- modules/tvmaze/models/episode.go
+++ modules/tvmaze/models/episode.go
@@ -2,18 +2,18 @@ package models
 
 // ShowEpisode is for data from prev/next links
 type ShowEpisode struct {
-	ID       int      `json:"id,omitempty"`
-	URL      string   `json:"url,omitempty"`
-	Name     string   `json:"name,omitempty"`
-	Season   int      `json:"season,omitempty"`
-	Number   int      `json:"number,omitempty"`
-	Airdate  string   `json:"airdate,omitempty"`
-	Airtime  string   `json:"airtime,omitempty"`
-	Airstamp string   `json:"airstamp,omitempty"`
-	Runtime  int      `json:"runtime,omitempty"`
-	Image    string   `json:"image,omitempty"`
-	Summary  string   `json:"summary,omitempty"`
-	Links    *epLinks `json:"_links"`
+	ID       int           `json:"id,omitempty"`
+	URL      string        `json:"url,omitempty"`
+	Name     string        `json:"name,omitempty"`
+	Season   int           `json:"season,omitempty"`
+	Number   int           `json:"number,omitempty"`
+	Airdate  string        `json:"airdate,omitempty"`
+	Airtime  string        `json:"airtime,omitempty"`
+	Airstamp string        `json:"airstamp,omitempty"`
+	Runtime  int           `json:"runtime,omitempty"`
+	Image    *EpisodeImage `json:"image,omitempty"`
+	Summary  string        `json:"summary,omitempty"`
+	Links    *epLinks      `json:"_links"`
 }
 
 type epLinks struct {
@@ -22,4 +22,10 @@ type epLinks struct {
 
 type epLink struct {
 	Href string `json:"href"`
+}
+
+// EpisodeImage is the shows images
+type EpisodeImage struct {
+	Medium   string `json:"medium,omitempty"`
+	Original string `json:"original,omitempty"`
 }
